24eb5d9d2c89d785067f8482e0cf671a1392f5f2
PlayDat3008 <titanv3585@gmail.com>
2020-03-11 20:30:31 +0100

    Rebrending
    MLP - POWER

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 // Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
 // Credits to expl0it, shellcode.team
 // GitHub: https://github.com/Lumm1t/obnoxious.club
+// GitHub: https://github.com/playday3008/playday3008.github.io															   
 
 var _app = function () {
     this.id = 0;
@@ -13,17 +14,17 @@ var _app = function () {
     this.shouldIgnoreVideo = false;
     this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
     this.brandDescription = [
-        "11 years old kids team", 
-        "pro roblox players", 
-        "xane ddosers", 
-        "trashtalkers", 
-        "esoterik fanboys", 
-        "hack vs hack team"
+        "Osiris",
+        "GOESP",
+        "PonySense",
+        "Pony Loves",
+        "We are girls from Equestria",
+        "OS hack team"
     ];
 
     this.titleChanger = function (text, delay) {
         text = text ||
-            ['Lummit', 'Neso', 'loljuxd', 'Vez', 'mannin', '=', 'obnoxious.club', 'Song name: KUSO GVKI - Cinnamon (feat. Apfel) (splash! Mag Premiere)'];
+            ['PlayDay', 'w1ldac3', 'Violet Scar', '=', 'playday3008.github.io', 'Song name: KUSO GVKI - Cinnamon (feat. Apfel) (splash! Mag Premiere)'];
         delay = delay || 2000;
 
         var counter = 0;
